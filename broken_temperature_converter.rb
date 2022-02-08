2fc767f4502dd7ecd71bb74f66e39c0dae5e0f87
Arthur-r19 <arl@ic.ufal.br>
2022-02-08 15:51:27 -0300

    first exercise

---
+++ broken_temperature_converter.rb
@@ -0,0 +1,6 @@
+# 1.1.6 checking for syntax erros
+celsius = 100
+fahrenheit = (celsius * 9 / 5 + 32
+puts "The result is "
+puts fahrenheit
+puts "."
---
+++ temperature_converter.rb
@@ -0,0 +1,6 @@
+# 1.1.5 writing and saving a simple program
+celsius = 100
+fahrenheit = (celsius * 9 / 5) + 32
+puts "The result is "
+puts fahrenheit
+puts "."
