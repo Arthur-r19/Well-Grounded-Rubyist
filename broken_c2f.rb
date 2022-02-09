7ebbfa467f6ed77d3c9d7349429a5981f3ee7711
Arthur-r19 <arl@ic.ufal.br>
2022-02-08 22:11:23 -0300

    commit

--- broken_c2f.rb
+++ broken_c2f.rb
@@ -1,4 +1,3 @@
-# 1.1.6 checking for syntax erros
 celsius = 100
 fahrenheit = (celsius * 9 / 5 + 32
 puts "The result is "
--- c2f.rb
+++ c2f.rb
@@ -1,4 +1,3 @@
-# 1.1.5 writing and saving a simple program
 celsius = 100
 fahrenheit = (celsius * 9 / 5) + 32
 puts "The result is "
